c3ad19745d4bc42b4cb0a9729167ddc2babc6efb
Evgeniy <114279264+dem-ov@users.noreply.github.com>
2023-03-18 11:43:41 +0200

    1.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,32 +1,44 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+const colorChanger = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      bodyBGColorChanger();
+    }, 1000);
+
+    refs.start.disabled = true;
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    refs.start.disabled = false;
+  },
+};
+
+refs.start.addEventListener('click', () => {
+  colorChanger.start();
+});
+
+refs.stop.addEventListener('click', () => {
+  colorChanger.stop();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const btnStart = document.querySelector('button[data-start]');
-const btnStop = document.querySelector('button[data-stop]');
-const body = document.querySelector('body');
-const colorValSpan = document.querySelector('.color');
-
-let timerId = null;
-
-// function to switch color to random
-
-const backgroundColorSwitcher = function () {
-  body.style.backgroundColor = getRandomHexColor();
-  body.style.color = getRandomHexColor();
-  colorValSpan.textContent = getRandomHexColor();
-};
-
-// start changing color
-
-btnStart.addEventListener('click', () => {
-  timerId = setInterval(backgroundColorSwitcher, 1000);
-  btnStart.disabled = true;
-});
-
-// stop changing color
-
-btnStop.addEventListener('click', () => {
-  clearInterval(timerId);
-  btnStart.disabled = false;
-});
+function bodyBGColorChanger() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
